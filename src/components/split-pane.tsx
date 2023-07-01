787bad4bbfcfcf267d811d039050d1ed70cb39c6
Raymar <raymarmontecs@gmail.com>
2023-07-01 21:04:26 +0800

    Implemented jankily working version of split pane

--- src/components/split-pane.tsx
+++ src/components/split-pane.tsx
@@ -1,15 +1,47 @@
+'use client'
+
+import { useRef, useState } from "react";
+
 export default function SplitPane({
     first, second
 }: {
     first: React.ReactNode,
     second: React.ReactNode
 }) {
+    const [isMouseDown, setIsMouseDown] = useState(false);
+    const [firstPaneBasis, setFirstPaneBasis] = useState('');
+
+    function handleDividerMouseDown(event: React.MouseEvent) {
+        setIsMouseDown(true);
+    }
+    
+    function handleDividerMove(event: React.MouseEvent) {
+        if (isMouseDown) {
+            setFirstPaneBasis(`${event.clientX}px`);
+        }
+        else {
+            handleEndMove(event);
+        }
+    }
+    
+    function handleEndMove(event: React.MouseEvent) {
+        setIsMouseDown(false);
+    }
+    
     return (
-        <div className="splitpane">
-            <div className="splitpane-first">
+        <div className="flex w-full h-96"
+            {...(isMouseDown && { onMouseMove: handleDividerMove, onMouseUp: handleEndMove })}
+        >
+            <div className="grow shrink-0 basis-[300px] flex w-full bg-red-500"
+                style={firstPaneBasis ? { flexBasis: firstPaneBasis } : {}}
+            >
                 {first}
             </div>
-            <div className="splitpane-second">
+            <div onMouseDown={handleDividerMouseDown}
+                className="grow-0 shrink-0 w-1 cursor-col-resize m-0">
+                &nbsp;
+            </div>
+            <div className="grow-0 shrink overflow-x-auto flex w-full bg-violet-500">
                 {second}
             </div>
         </div>
