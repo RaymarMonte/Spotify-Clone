ba118945e04c365653dd8379831d8a31decb45e2
Raymar <raymarmontecs@gmail.com>
2023-08-11 14:16:33 +0800

    Added circular font initialization on layout.tsx. Reverted prior changes to next.config.js

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,62 @@
 import './globals.css'
-import LocalFont from 'next/font/local'
+import localFont from 'next/font/local'
 import { NowPlayingBar } from '@/components/now-playing-bar'
 import { Dashboard } from '@/components/dashboard'
 
-const circular = LocalFont({ src: './my-font.woff2' })
-// var(--font-family,CircularSp,CircularSp-Arab,CircularSp-Hebr,CircularSp-Cyrl,CircularSp-Grek,CircularSp-Deva,var(--fallback-fonts,sans-serif))
+const circular = localFont({
+  src: [
+    {
+      path: './circular/CircularStd-Black.otf',
+      weight: '900',
+      style: 'normal',
+    },
+    {
+      path: './circular/CircularStd-BlackItalic.otf',
+      weight: '900',
+      style: 'italic',
+    },
+    {
+      path: './circular/CircularStd-Bold.otf',
+      weight: '700',
+      style: 'normal',
+    },
+    {
+      path: './circular/CircularStd-BoldItalic.otf',
+      weight: '700',
+      style: 'italic',
+    },
+    {
+      path: './circular/CircularStd-Book.otf',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: './circular/CircularStd-BookItalic.otf',
+      weight: '400',
+      style: 'italic',
+    },
+    {
+      path: './circular/CircularStd-Light.otf',
+      weight: '300',
+      style: 'normal',
+    },
+    {
+      path: './circular/CircularStd-LightItalic.otf',
+      weight: '300',
+      style: 'italic',
+    },
+    {
+      path: './circular/CircularStd-Medium.otf',
+      weight: '500',
+      style: 'normal',
+    },
+    {
+      path: './circular/CircularStd-MediumItalic.otf',
+      weight: '500',
+      style: 'italic',
+    },
+  ]
+})
 
 export const metadata = {
   title: 'Beat Remedy',
@@ -18,7 +70,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="select-none">
-      <body >
+      <body className={circular.className}>
         <div className="flex flex-col h-screen p-2 text-white bg-black">
           <div className="grow">
             <Dashboard />
