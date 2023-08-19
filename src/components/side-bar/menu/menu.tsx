528f1131ec26e8c50bc548143e8e4db72450fcb5
Raymar <raymarmontecs@gmail.com>
2023-08-19 15:32:24 +0800

    Added activated menu icons

--- src/components/side-bar/menu/menu.tsx
+++ src/components/side-bar/menu/menu.tsx
@@ -1,13 +1,18 @@
 import { HomeSVGPath } from "../../icons/home-svg-path";
 import { MenuItem } from "./menu-item";
 import { SearchSVGPath } from "../../icons/search-svg-path";
+import { useState } from "react";
+import { ActivatedHomeSVGPath } from "@/components/icons/activated-home-svg-path";
+import { ActivatedSearchSVGPath } from "@/components/icons/activated-search-svg-path";
 
 export function Menu() {
+    const [isHomeActivated, setIsHomeActivated] = useState(true);
+    const [isSearchActivated, setIsSearchActivated] = useState(true);
     return (
         <div className="px-3 py-2 rounded-lg bg-[#121212]">
             <ul>
-                <MenuItem icon={<HomeSVGPath />} label="Home" isLabelShown={false} />
-                <MenuItem icon={<SearchSVGPath />} label="Search" isLabelShown={false} />
+                <MenuItem icon={isHomeActivated ? <ActivatedHomeSVGPath /> : <HomeSVGPath />} label="Home" isLabelShown={true} />
+                <MenuItem icon={isSearchActivated ? <ActivatedSearchSVGPath /> : <SearchSVGPath />} label="Search" isLabelShown={true} />
             </ul>
         </div>
     )
