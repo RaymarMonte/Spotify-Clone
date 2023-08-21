92b099db8138ad498b0892b0ad34ce12ec5e124d
Raymar <raymarmontecs@gmail.com>
2023-08-21 22:27:31 +0800

    Added header tags to library component

--- src/components/side-bar/library.tsx
+++ src/components/side-bar/library.tsx
@@ -2,9 +2,14 @@ export function Library() {
     return (
         <div className="flex flex-col w-full h-0 min-h-full rounded-lg bg-[#121213]">
             <div className="flex-none">
-                <div className="h-32">
-                    Your Library
-                </div>
+                <header className="py-2 px-4">
+                    <div className="flex">
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                    </div>
+                </header>
             </div>
             <div className="scrollable grow divide-y">
                 <div className="w-full h-32 flex"></div>
