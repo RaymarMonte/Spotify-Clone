77cbeb0cc024378874e96148aed9c4045a8878b5
Raymar <raymarmontecs@gmail.com>
2023-08-22 20:08:05 +0800

    Renamed icons to remove specification that they're a path. - Added some more icons

---
+++ src/components/icons/grid-view-icon.tsx
@@ -0,0 +1,5 @@
+export function GridViewIcon() {
+    return (
+        <path d="M1 1h6v6H1V1zm1.5 1.5v3h3v-3h-3zM1 9h6v6H1V9zm1.5 1.5v3h3v-3h-3zM9 1h6v6H9V1zm1.5 1.5v3h3v-3h-3zM9 9h6v6H9V9zm1.5 1.5v3h3v-3h-3z"></path>
+    )
+}
--- src/components/side-bar/menu/menu.tsx
+++ src/components/side-bar/menu/menu.tsx
@@ -1,9 +1,9 @@
-import { HomeSVGPath } from "../../icons/home-svg-path";
+import { HomeIcon } from "../../icons/home-icon";
+import { SearchIcon } from "../../icons/search-icon";
+import { ActivatedHomeIcon } from "@/components/icons/activated-home-icon";
+import { ActivatedSearchIcon } from "@/components/icons/activated-search-icon";
 import { MenuItem } from "./menu-item";
-import { SearchSVGPath } from "../../icons/search-svg-path";
 import { useState } from "react";
-import { ActivatedHomeSVGPath } from "@/components/icons/activated-home-svg-path";
-import { ActivatedSearchSVGPath } from "@/components/icons/activated-search-svg-path";
 
 export function Menu() {
     const [isHomeActivated, setIsHomeActivated] = useState(true);
@@ -11,8 +11,8 @@ export function Menu() {
     return (
         <div className="px-3 py-2 rounded-lg bg-[#121212]">
             <ul>
-                <MenuItem icon={isHomeActivated ? <ActivatedHomeSVGPath /> : <HomeSVGPath />} label="Home" isLabelShown={true} />
-                <MenuItem icon={isSearchActivated ? <ActivatedSearchSVGPath /> : <SearchSVGPath />} label="Search" isLabelShown={true} />
+                <MenuItem icon={isHomeActivated ? <ActivatedHomeIcon /> : <HomeIcon />} label="Home" isLabelShown={true} />
+                <MenuItem icon={isSearchActivated ? <ActivatedSearchIcon /> : <SearchIcon />} label="Search" isLabelShown={true} />
             </ul>
         </div>
     )
