c9cf2a88aff87baa9392b8965a1cca2e4a2af65e
Raymar <raymarmontecs@gmail.com>
2023-08-22 20:08:27 +0800

    Added right-side icons to library header

--- src/components/side-bar/library.tsx
+++ src/components/side-bar/library.tsx
@@ -1,13 +1,29 @@
+import { CreateIcon } from "../icons/create-icon";
+import { GridViewIcon } from "../icons/grid-view-icon";
+import { ReduceIcon } from "../icons/reduce-icon";
+
 export function Library() {
     return (
         <div className="flex flex-col w-full h-0 min-h-full rounded-lg bg-[#121213]">
             <div className="flex-none">
                 <header className="py-2 px-4">
-                    <div className="flex">
+                    <div className="flex gap-2">
                         <div></div>
-                        <div></div>
-                        <div></div>
-                        <div></div>
+                        <div className="p-2">
+                            <svg height="16" width="16" className="fill-white">
+                                <CreateIcon />
+                            </svg>
+                        </div>
+                        <div className="p-2">
+                            <svg height="16" width="16" className="fill-white">
+                                <GridViewIcon />
+                            </svg>
+                        </div>
+                        <div className="p-2">
+                            <svg height="16" width="16" className="fill-white">
+                                <ReduceIcon />
+                            </svg>
+                        </div>
                     </div>
                 </header>
             </div>
